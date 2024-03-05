93249cc2e8ab9873c57c373e4d038db5fda49041
Brandon Huerta <bran.7594@gmail.com>
2024-03-04 23:05:49 -0600

    cambio en spanish

--- obj/Debug/net8.0/blazorAzure.AssemblyInfo.cs
+++ obj/Debug/net8.0/blazorAzure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("blazorAzure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a16cefca14500849d7ed5909591600dd644743f")]
 [assembly: System.Reflection.AssemblyProductAttribute("blazorAzure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("blazorAzure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
